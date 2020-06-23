59b28b2ff7da811dbc9c63f311dc49a48b950442
Simon Burkhardt <simon.burkhardt.prime@gmail.com>
2020-06-23 10:55:49 +0200

    first implementation of new website

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # LSZHMovements
 web API for ZRH/LSZH Zürich Airport Airport arrivals/departures Table
+
+---
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+[![volkswagen status](https://auchenberg.github.io/volkswagen/volkswargen_ci.svg?v=1)](https://github.com/auchenberg/volkswagen)
+
+This script downloads the `json` table from the new LSZH website: [flughafen-zuerich.ch](https://dxp-fds.flughafen-zuerich.ch/flights) and parses it into two `timetable.json` files.
+
+
+### Usage
+
+Use the `-o` parameter to specify the output directory.
+
+```bash
+python3 get-lszh.py -o zrh/
+```
+
+Outputs: `zrh/arrivals.timetable.json` and `zrh/departures.timetable.json` 
+
+### Example Output
+
+```json
+{
+  "data": [
+    {
+      "id": 776369,
+      "flightType": "A",
+      "SDT": "2020-06-23T00:00:00Z",
+      "STA": "2020-06-23T13:50:00Z",
+      "ETA": "2020-06-23T13:01:00Z",
+      "TER": "2",
+      "FLC": "LX",
+      "airlineLogo": "https://www.flughafen-zuerich.ch/-/jssmedia/airport/portal/logos/airline/swiss.svg?vs=1",
+      "FLN": "189",
+      "REG": "HBJNH",
+      "statusCode": 10,
+      "statusTextDe": "Fracht",
+      "statusTextEn": "Cargo",
+      "airportName": "International Airport",
+      "cityDe": "Shanghai",
+      "cityEn": "Shanghai",
+      "search": "LX189 LX 189 SWISS PVG  International Airport Shanghai Shanghai       ",
+      "POR": "PVG",
+      "ICT": "B77W",
+      "TYP": "777",
+      "TYS": "77W",
+      "model": "777-300",
+      "manufacturerName": "Boeing",
+      "isCommercial": false,
+      "airline": "SWISS"
+    }
+  ]
+}
+```
+
+
+---
+
+### Statement
+
+> "Wir freuen uns, dass Sie unsere neue Webseite bereits ausprobiert haben und danken Ihnen für Ihre Anfrage für einen API-Zugang auf unsere Homepage.
+
+> Bezüglich der detaillierten Informationen über den Flugzeugtyp und Registration der Flugzeuge möchten wir Ihnen mitteilen, dass wir diese Funktion für unsere Aviatikfans verbessert und erweitert haben. So werden beispielsweise neu auch Frachtflüge angezeigt. Sie können sich ein Profil erstellen und erhalten detaillierte, geräteübergreifende Informationen oder Sie stellen sich den Spottermodus zeitlich begrenzt ein. Diese sowie weiterführende Informationen für Spotter finden Sie unter dem folgenden Link: [www.flughafen-zuerich.ch/de/passagiere/erleben/erlebnisse/spotter-informationen](http://www.flughafen-zuerich.ch/de/passagiere/erleben/erlebnisse/spotter-informationen).
+
+> Wir sind überzeugt, dass wir so für die Aviatikfans eine gute Möglichkeit gefunden haben, um Informationen abrufen zu können und hoffen, dass Ihnen die neue Webseite künftig von besserem Nutzen sein wird."
+
+
+---
+
+LICENSE
+
+Apache License 2.0
+
+(c) 2020 Simon Burkhardt
+
+
+
+
+
---
+++ get-lszh.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 10:20:09 2020
+
+@author: simon
+"""
+
+import requests					# requesting webpages
+import json
+import optparse
+
+parser = optparse.OptionParser('get-lszh')
+parser.add_option('-o', '--outdir',	dest='outdir', help='[optional] output directory')
+parser.add_option('-s', '--single-file',	action='store_true', dest='single', help='[optional] save to single file')
+parser.add_option('-p', '--spotter-only',	action='store_true', dest='spotter', help='[optional] only save the spotter relevant flights')
+(opts, args) = parser.parse_args()
+
+headers = { 'Host': 'dxp-fds.flughafen-zuerich.ch',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'DNT': '1',
+            'Upgrade-Insecure-Requests': '1',
+            'Cache-Control': 'max-age=0'}
+
+def getLSZH(tabletyp, spotter=False):
+    requrl = 'https://dxp-fds.flughafen-zuerich.ch/flights'
+    # website currently does not require to send the headers
+    response = requests.get(requrl, headers=headers)
+    
+    if(response.status_code != 200):
+        print('error: Website returned status code: ' + str(response.status_code))
+    
+    timetable = []
+    if("flightType" in response.text):
+        rawtable = json.loads(response.text)
+        for flight in rawtable:
+            if('arr' in tabletyp.lower()):
+                if('a' in flight['flightType'].lower()):
+                    timetable.append(flight)
+            else:
+                if('d' in flight['flightType'].lower()):
+                    timetable.append(flight)    
+    return timetable
+
+def writeJsonFile(filename, data):
+    with open(str(filename), 'w') as outfile:
+        json.dump(data, outfile)
+
+#%%
+def main():
+    try:
+        outdir = '.'
+        if(opts.outdir is not None):
+            outdir = opts.outdir
+            
+        # opts.spotter = True
+        
+        fileapp = '.json'
+        if(opts.spotter is True):
+            fileapp = '.spotter.json'
+ 
+        table = {}
+        table['data'] = []
+        if(opts.single is not None):
+            table['data'] = getLSZH('arr', opts.spotter)
+            table['data'] = table['data'] + getLSZB('dep')
+            # sort by arrital time
+            table['data'] = sorted(table['data'], key=lambda k: k['scheduledTime']) 
+            writeJsonFile(outdir + '/timetable' + fileapp, table)
+        else:
+            table['data'] = getLSZH('arr', opts.spotter)
+            writeJsonFile(outdir + '/arrivals.timetable' + fileapp, table)
+            table['data'] = getLSZH('dep', opts.spotter)
+            writeJsonFile(outdir + '/departures.timetable' + fileapp, table)
+        
+    except KeyboardInterrupt:
+        print('')
+        exit(0)
+
+if __name__ == '__main__':
+	main()
+    
+    
